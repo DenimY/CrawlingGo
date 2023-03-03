2bf669a60259d47cded5c54af34982199903ac8f
kyungmuYou <ykm3976@gmail.com>
2023-03-03 11:45:31 +0900

    ️go routine 사용해서 여러페이지를 크롤링하려고 했으나 페이지 오류로 실패,
    navigate에 문제가 있는지 혹은 Context를 새로 new로 만들어야 하는지 확인 중

--- src/modules/crawling.go
+++ src/modules/crawling.go
@@ -3,12 +3,21 @@ package modules
 import (
 	"context"
 	model "crawling.com/models"
+	"fmt"
 	"github.com/chromedp/chromedp"
 	"log"
+	"math"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
+
+var LoginUrl string
+var GoodsUrl string
+var PageCount = 500
+var Done int
 
 func Crawling(e *model.Environment) []model.GoodsInfo {
 
@@ -18,37 +27,109 @@ func Crawling(e *model.Environment) []model.GoodsInfo {
 	)
 	defer cancel()
 
-	pageCount := 500
+	PageCount = 500
 
-	loginUrl := e.BaseUrl + e.Login.Url
-	goodsUrl := e.BaseUrl + e.Goods.Url
+	LoginUrl = e.BaseUrl + e.Login.Url
+	GoodsUrl = e.BaseUrl + e.Goods.Url
 
-	var res string
 	id := e.Login.Id
 	pwd := e.Login.Pwd
 
-	err := chromedp.Run(ctx, doLogin(loginUrl, id, pwd, &res))
+	log.Printf("login start")
+	err := chromedp.Run(ctx, doLogin(LoginUrl, id, pwd))
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("login suc")
 	var infoList []model.GoodsInfo
 
+	// 먼저 이동
 	var totalCount string
 	chromedp.Run(ctx,
-		chromedp.Navigate(goodsUrl),
+		chromedp.Navigate(GoodsUrl),
+		chromedp.WaitVisible(`//*[@id="frmList"]/div[2]/table`),
 		chromedp.Text(`//*[@id="frmSearchGoods"]/div[4]/div[1]/strong[1]`, &totalCount),
 	)
 
-	// 먼저 이동
 	t, _ := strconv.Atoi(strings.Replace(totalCount, ",", "", -1))
-	totalPage := t / pageCount
-	totalPage = 1
-	for pn := 1; pn <= totalPage; pn++ {
+
+	/*slicePage := (t / 500)
+	go func(msg int) {
+		defer wait.Done() //끝나면 .Done() 호출
+		fmt.Println(msg)
+	}(slicePage)
+
+	go func() {
+		defer wait.Done() //끝나면 .Done() 호출
+		fmt.Println("Hello")
+	}()*/
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	log.Printf(string(runtime.NumCPU()))
+
+	var wait sync.WaitGroup
+
+	totalPage := int(math.Ceil(float64(t) / float64(PageCount)))
+	//totalPage = 1 //test code
+	excelFileName := time.Now().Format("20060102150405")
+	sliceCnt := 1
+	goSlice := int(math.Floor(float64(totalPage) / float64(sliceCnt)))
+
+	fmt.Println(goSlice)
+	//startPn := 1
+	//endPn := 0
+	wait.Add(sliceCnt)
+
+	for j := 0; j < sliceCnt; j++ {
+		go func(n int) {
+			defer wait.Done()
+
+			/*log.Printf("Routine-%d \n", n)
+			ctxn, cancel := chromedp.NewContext(
+				context.Background(),
+			)
+			defer cancel()
+
+			PageCount = 500
+
+			LoginUrl = e.BaseUrl + e.Login.Url
+			GoodsUrl = e.BaseUrl + e.Goods.Url
+
+			log.Printf("Routine-%d login try\n", n)
+			err := chromedp.Run(ctxn, doLogin(LoginUrl, id, pwd))
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Routine-%d login suc\n", n)*/
+
+			//
+
+			startPn := n * (goSlice)
+			if startPn < 1 {
+				startPn = 1
+			}
+			endPn := startPn + (goSlice - 1)
+			//fmt.Println(strconv.Itoa(startPn) + "-" + strconv.Itoa(endPn))
+			//fmt.Println(n)
+
+			log.Printf("Routine-%d crawlingGo\n", n)
+			crawlingGo(ctx, startPn, endPn, n, fmt.Sprintf(excelFileName+"_", strconv.Itoa(startPn), "-", strconv.Itoa(endPn)))
+
+		}(j)
+	}
+
+	wait.Wait()
+
+	log.Println("clear go routine")
+
+	/*routineCnt = 1
+	pn := 1 // start page
+	for ; pn <= totalPage; pn++ {
+		le := routineCnt * (totalPage / 12)
 		// page 이동
 		if pn != 1 {
 			chromedp.Run(ctx,
-				chromedp.Navigate(strings.Replace(goodsUrl, "?page=1&&", "?page="+strconv.Itoa(pn)+"&&", 1)),
+				chromedp.Navigate(strings.Replace(GoodsUrl, "?page=1&&", "?page="+strconv.Itoa(pn)+"&&", 1)),
 			)
 		}
 
@@ -60,13 +141,68 @@ func Crawling(e *model.Environment) []model.GoodsInfo {
 				break
 			}
 			//log.Printf("( %d / %d ) : %s, %s, %s, %s, %s ", (pn-1)*pageCount+i, t, info.IdNum, info.Brand, info.Supplier, info.GoodsCode, info.Img)
+			log.Printf("( %d / %d )", (pn-1)*PageCount+i, t)
 			infoList = append(infoList, info)
 		}
 		log.Printf("( %d / %d ) ", pn, totalPage)
-	}
+
+		if le == (pn) || pn == totalPage {
+			GoodsOutput(infoList, fmt.Sprintf(excelFileName+"_", saveCnt))
+			// 초기화
+			infoList = []model.GoodsInfo{}
+			routineCnt++
+		}
+	}*/
 
 	return infoList
 
+}
+
+func crawlingGo(ctx context.Context, startPage, endPage, routineCnt int, excelFileName string) error {
+	pn := startPage
+	saveCnt := 1
+	//t, _ := strconv.Atoi(strings.Replace(totalCount, ",", "", -1))
+	var infoList []model.GoodsInfo
+	for ; pn <= endPage; pn++ {
+		// page 이동
+		url := strings.Replace(GoodsUrl, "?page=1&&", "?page="+strconv.Itoa(pn)+"&&", 1)
+		err := chromedp.Run(ctx,
+			chromedp.WaitVisible(`//*[@id="frmList"]/div[2]/table`),
+			chromedp.Navigate(strings.Replace(GoodsUrl, "?page=1&&", "?page="+strconv.Itoa(pn)+"&&", 1)),
+		)
+
+		if err != nil {
+			log.Printf("navigate err = %v\n: %v\n", url, err)
+			log.Printf("navigate err = %v: %v\n", startPage, err)
+			return nil
+		}
+
+		for i := 1; i <= 500; i++ {
+			var info model.GoodsInfo
+			err := chromedp.Run(ctx, getGoodsInfo(i, &info))
+			if err != nil {
+				log.Fatal(err)
+				break
+			}
+			//log.Printf("( %d / %d ) : %s, %s, %s, %s, %s ", (pn-1)*pageCount+i, t, info.IdNum, info.Brand, info.Supplier, info.GoodsCode, info.Img)
+			log.Printf("Routine-%d ( %d / %d )", routineCnt, (pn-1)*PageCount+i, endPage)
+			infoList = append(infoList, info)
+		}
+		log.Printf("Routine-%d : ( %d / %d ) ", routineCnt, pn, endPage)
+
+		//le := saveCnt * (endPage / 50)
+		//if le == (pn) || pn == endPage {
+		GoodsOutput(infoList, fmt.Sprintf(excelFileName+"_", saveCnt))
+		// 초기화
+		infoList = []model.GoodsInfo{}
+		saveCnt++
+		//}
+
+		//GoodsOutput(infoList, fmt.Sprintf(excelFileName+"_", routineCnt))
+
+	}
+
+	return nil
 }
 
 type WebDriver struct {
@@ -81,30 +217,29 @@ func checkErr(err error) {
 
 func getGoodsInfo(count int, info *model.GoodsInfo) chromedp.Tasks {
 	return chromedp.Tasks{
+		chromedp.WaitVisible(`//*[@id="frmList"]/div[2]/table`),
 		chromedp.Text(`//*[@id="frmList"]/div[2]/table/tbody/tr[`+strconv.Itoa(count)+"]/td[2]", &info.IdNum),
 		chromedp.Text(`//*[@id="frmList"]/div[2]/table/tbody/tr[`+strconv.Itoa(count)+"]/td[3]", &info.Supplier),
 		chromedp.Text(`//*[@id="frmList"]/div[2]/table/tbody/tr[`+strconv.Itoa(count)+"]/td[4]", &info.GoodsCode),
 		chromedp.Text(`//*[@id="frmList"]/div[2]/table/tbody/tr[`+strconv.Itoa(count)+"]/td[5]", &info.Brand),
-		chromedp.Evaluate(`document.querySelector("#frmList > div.table-responsive > table > tbody > tr:nth-child(1) > td.width-2xs.center > a > img").getAttribute('src')`, &info.Img),
+		chromedp.Evaluate(`document.querySelector("#frmList > div.table-responsive > table > tbody > tr:nth-child(`+strconv.Itoa(count)+`) > td.width-2xs.center > a > img").getAttribute('src')`, &info.Img),
 		chromedp.Text(`//*[@id="frmList"]/div[2]/table/tbody/tr[`+strconv.Itoa(count)+"]/td[9]/div[1]/a", &info.Name),
 		chromedp.Text(`//*[@id="frmList"]/div[2]/table/tbody/tr[`+strconv.Itoa(count)+"]/td[10]", &info.RetailPrice),
 		chromedp.Text(`//*[@id="frmList"]/div[2]/table/tbody/tr[`+strconv.Itoa(count)+"]/td[11]", &info.SellingPrice),
 	}
 }
-func doLogin(loginUrl string, id string, pwd string, res *string) chromedp.Tasks {
-	log.Printf("login try")
+func doLogin(loginUrl string, id string, pwd string) chromedp.Tasks {
 	return chromedp.Tasks{
 		//chromedp.Evaluate(`window.scrollTo(0,document.querySelector("body ytd-app div#content").clientHeight); document.querySelector("body ytd-app div#content").clientHeight;`, &newHeight),
 		chromedp.Navigate(loginUrl),
-		//chromedp.WaitVisible(`body > footer`),
+		//chromedp.WaitVisible(`body`),
 		//chromedp.WaitVisible(`#frmLogin #login`, chromedp.ByID),
 		//chromedp.SendKeys(`#frmLogin #login`, id, chromedp.ByID),
 		chromedp.SendKeys(`//*[@id="login"]`, id),
 		chromedp.SendKeys(`//*[@id="frmLogin"]/table/tbody/tr/td/div/div[1]/div[1]/div[2]/input`, pwd),
 		chromedp.Click(`//*[@id="frmLogin"]/table/tbody/tr/td/div/div[1]/div[2]/input`),
 		//chromedp.Click(`//*[@id="loginFrm"]/button`),
-		chromedp.Sleep(4 * time.Second),
+		chromedp.Sleep(5 * time.Second),
 		//chromedp.Text(`/html/body/div[2]/div/div[1]/div/span`, res),
-		chromedp.Text(`html`, res),
 	}
 }
--- src/modules/excel.go
+++ src/modules/excel.go
@@ -1,17 +1,25 @@
 package modules
 
 import (
+	"bytes"
 	model "crawling.com/models"
+	"errors"
 	"fmt"
 	"github.com/xuri/excelize/v2"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
 	"log"
-	"strconv"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 )
 
 func excel() {
 
 }
-func GoodsOutput(dataList []model.GoodsInfo) {
+func GoodsOutput(dataList []model.GoodsInfo, fileName string) {
 	f := excelize.NewFile()
 	defer func() {
 		if err := f.Close(); err != nil {
@@ -20,30 +28,98 @@ func GoodsOutput(dataList []model.GoodsInfo) {
 	}()
 
 	sheetName := "Sheet1"
-	index, err := f.NewSheet(sheetName)
+	_, err := f.NewSheet(sheetName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for index, data := range dataList {
-		cellNum := strconv.Itoa(index)
-		f.SetCellValue(sheetName, "A"+cellNum, data.IdNum)
-		f.SetCellValue(sheetName, "B"+cellNum, data.Supplier)
-		f.SetCellValue(sheetName, "C"+cellNum, data.GoodsCode)
-		f.SetCellValue(sheetName, "D"+cellNum, data.Brand)
-		f.SetCellValue(sheetName, "E"+cellNum, data.Img)
-		f.SetCellValue(sheetName, "F"+cellNum, data.Name)
-		f.SetCellValue(sheetName, "G"+cellNum, data.RetailPrice)
-		f.SetCellValue(sheetName, "H"+cellNum, data.SellingPrice)
+	log.Println("start start excel")
+	cellCount := 0
+	for _, data := range dataList {
+		cellCount++
+		// 12개로 분할하여 저장
+		//f.SetColWidth("Sheet1", "E", "H", 20)
+		f.SetRowHeight(sheetName, cellCount, 50)
+		f.SetCellValue(sheetName, fmt.Sprint("A", cellCount), data.IdNum)
+		f.SetCellValue(sheetName, fmt.Sprint("B", cellCount), data.Supplier)
+		f.SetCellValue(sheetName, fmt.Sprint("C", cellCount), data.GoodsCode)
+		f.SetCellValue(sheetName, fmt.Sprint("D", cellCount), data.Brand)
+		addPicture(f, sheetName, data.GoodsCode, fmt.Sprint("E", cellCount), data.Img)
+		f.SetCellValue(sheetName, fmt.Sprint("F", cellCount), data.Img)
+		f.SetCellValue(sheetName, fmt.Sprint("G", cellCount), data.Name)
+		f.SetCellValue(sheetName, fmt.Sprint("H", cellCount), data.RetailPrice)
+		f.SetCellValue(sheetName, fmt.Sprint("U", cellCount), data.SellingPrice)
 	}
 	//통합 문서에 대 한 기본 워크시트를 설정 합니다
-	f.SetActiveSheet(index)
-	e := saveFile(f, "newFile")
 
+	e := saveFile(f, fileName)
 	if e != nil {
 		log.Printf("excel save failure : %s\n", e)
+	} else {
+		log.Printf("save excel : %v", fileName)
 	}
+
+}
+
+func GoodsOutputSlice(dataList []model.GoodsInfo) {
+	f := excelize.NewFile()
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	sheetName := "Sheet1"
+	_, err := f.NewSheet(sheetName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	log.Println("start start excel")
+	count := 1
+	cellCount := 0
+	fullLen := len(dataList)
+	for index, data := range dataList {
+		// 12개로 분할하여 저장
+		le := count * (fullLen / 12)
+		cellCount++
+		//f.SetColWidth("Sheet1", "E", "H", 20)
+		f.SetRowHeight(sheetName, cellCount, 50)
+		f.SetCellValue(sheetName, fmt.Sprint("A", cellCount), data.IdNum)
+		f.SetCellValue(sheetName, fmt.Sprint("B", cellCount), data.Supplier)
+		f.SetCellValue(sheetName, fmt.Sprint("C", cellCount), data.GoodsCode)
+		f.SetCellValue(sheetName, fmt.Sprint("D", cellCount), data.Brand)
+		addPicture(f, sheetName, data.GoodsCode, fmt.Sprint("E", cellCount), data.Img)
+		f.SetCellValue(sheetName, fmt.Sprint("F", cellCount), data.Img)
+		f.SetCellValue(sheetName, fmt.Sprint("G", cellCount), data.Name)
+		f.SetCellValue(sheetName, fmt.Sprint("H", cellCount), data.RetailPrice)
+		f.SetCellValue(sheetName, fmt.Sprint("U", cellCount), data.SellingPrice)
+
+		if le == (index+1) || index+1 == fullLen {
+			cellCount = 0
+			f.SetActiveSheet(index)
+			e := saveFile(f, fmt.Sprint(time.Now().Format("20060102150405")+"_", count))
+			if e != nil {
+				log.Printf("excel save failure : %s\n", e)
+			}
+			f = excelize.NewFile()
+			defer func() {
+				if err := f.Close(); err != nil {
+					log.Println(err)
+				}
+			}()
+			_, err = f.NewSheet(sheetName)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			count++
+		}
+	}
+	//통합 문서에 대 한 기본 워크시트를 설정 합니다
+
 }
 func uploadFile() {
 
@@ -55,4 +131,85 @@ func updateFile() {
 
 func saveFile(file *excelize.File, fileName string) error {
 	return file.SaveAs(fileName + ".xlsx")
+}
+
+func buildFileName() {
+	fullUrlFile := "https://i.imgur.com/m1UIjW1.jpg"
+	fileUrl, e := url.Parse(fullUrlFile)
+	if e != nil {
+		panic(e)
+	}
+
+	path := fileUrl.Path
+	segments := strings.Split(path, "/")
+
+	fileName := segments[len(segments)-1]
+	println(fileName)
+}
+
+/*
+	func testpick() error {
+		//fullUrlFile := "https://i.imgur.com/m1UIjW1.jpg"
+		fullUrlFile := "https://pjh5023.cdn-nhncommerce.com/data/goods/23/02/09/1000326716/t50_1000133769_list_022.jpg"
+		fileName := "m1UIjW1.jpg"
+
+		r, e := http.Get(fullUrlFile)
+		if e != nil {
+			panic(e)
+		}
+		defer r.Body.Close()
+		//buildFileName()
+		// Create distination
+		f, e := os.Create(fileName) // "m1UIjW1.jpg"
+		if e != nil {
+			panic(e)
+		}
+		defer f.Close()
+		// Fill distination with content
+		n, e := f.ReadFrom(r.Body)
+		if e != nil {
+			panic(e)
+		}
+		fmt.Println("File size: ", n)
+
+		return nil
+	}
+*/
+func addPicture(f *excelize.File, sheet, imgName, cell, imgUrl string) error {
+
+	extension := imgUrl[strings.LastIndex(imgUrl, "."):len(imgUrl)]
+	r, err := http.Get(imgUrl)
+	if err != nil {
+		log.Printf("http.Get err: %v ", err)
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	if r.StatusCode != 200 {
+		return errors.New("Received non 200 response code")
+	}
+
+	buf := &bytes.Buffer{}
+	//buf := new(bytes.Buffer)
+	n, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		fmt.Println("File size1: ", n)
+		fmt.Println("buf readForm err: ", err)
+	}
+
+	if err := f.AddPictureFromBytes(sheet, cell, imgName, strings.ToLower(extension), buf.Bytes(),
+		&excelize.GraphicOptions{
+			AutoFit: true,
+			//ScaleX:  0.1,
+			//ScaleY:  0.1,
+			OffsetX: 15,
+			OffsetY: 10,
+			//Hyperlink:     imgUrl,
+			//HyperlinkType: "External",
+			Positioning: "oneCell",
+		}); err != nil {
+		log.Printf("AddPictureFromBytes err:  %v ", err)
+	}
+
+	return nil
 }
